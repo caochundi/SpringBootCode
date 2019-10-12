1491eae0e6bb1c4910bdc012290f34e912574b80
caochundi <2879170213@qq.com>
2019-10-12 14:37:23 +0800

    init project
    初始化微信公众号后台程序

---
+++ wxinteface/src/main/java/org/ccd/wxinteface/WxintefaceApplication.java
@@ -0,0 +1,13 @@
+package org.ccd.wxinteface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxintefaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxintefaceApplication.class, args);
+    }
+
+}
